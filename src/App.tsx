75c1fe57e163268d8508175877c28ea1095dde7a
Bhavya <bhavyapittala1@gmail.com>
2023-05-10 01:14:19 -0400

    Display Platform Icons

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
 				<NavBar />
 			</GridItem>
 			<Show above="lg">
-				<GridItem area="aside">Aside</GridItem>
+				<GridItem area="aside"> </GridItem>
 			</Show>
 			<GridItem area="main">
 				<GameGrid />
